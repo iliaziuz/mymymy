20785e4b2f8ec086e79013b9dbcf5b09c4ec67e6
iliaziuz <tiekhmurov@gmail.com>
2021-06-12 22:36:51 +0300

    update news

--- first/news/views.py
+++ first/news/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News
+from .forms import NewsForm
 
 def news(request):
     news = News.objects.all()
     return render(request, 'news/news.html', {'news': news})
+
+
+def create(request):
+    error = ''
+    if request.method =='POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news')
+        else:
+            error = 'Форма была неверной'
+
+
+    form = NewsForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', data)
